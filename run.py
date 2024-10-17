c68b4667b1dc67f8e4067bc9c59f79977951dd69
Pedro Gonzalez Soro <pedrogonzalezsoro@192.168.1.4>
2024-10-17 19:16:54 -0300

    vercel

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ from app import create_app
 
 app = create_app()
 
+# hello world route
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
 if __name__ == "__main__":
     logging.info("Flask app started")
     app.run(host="0.0.0.0", port=8000)
